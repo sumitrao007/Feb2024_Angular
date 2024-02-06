f546dc70726b8c83ba286f4fd88d2768e616bb71
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2024-02-06 20:19:29 +0530

    Day01

---
+++ Typescript/Day01/demo1.js
@@ -0,0 +1,3 @@
+console.log("Hello World...");
+console.log('Welcome to Angular module');
+console.log("\n    Hopes so u r Enjoying Session\n");
---
+++ Typescript/Day01/demo1.ts
@@ -0,0 +1,14 @@
+console.log("Hello World...");
+console.log('Welcome to Angular module');
+console.log(`
+    Hopes so u r Enjoying Session
+`);
+
+// var => scope global 
+// let => scope => within nearest block {}
+// const => global as well as local 
+
+var temp,a1;
+let a2;
+const pi=3.14;
+
